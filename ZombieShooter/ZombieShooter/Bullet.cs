0a50a055bb1ee386ae5d718c9c9536f13b78d838
Nikola-Kostov <niko_kostov@yahoo.com>
2020-06-09 21:50:47 +0200

    making bullets and edited movement

--- ZombieShooter/ZombieShooter/Bullet.cs
+++ ZombieShooter/ZombieShooter/Bullet.cs
@@ -1,13 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ZombieShooter
 {
     public class Bullet
     {
+        public string direction { get; set; }
+        public int bulletLeft { get; set; }
+        public int bulletTop { get; set; }
 
+        private int speed = 20;
+        private PictureBox bullet = new PictureBox();
+        private Timer bulletTimer = new Timer();
+
+        public void makeBullet (Form form) // pravenje bullet
+        {
+            bullet.BackColor = Color.White;
+            bullet.Size = new Size(5, 5);
+            bullet.Tag = "bullet";
+            bullet.Left = bulletLeft;
+            bullet.Top = bulletTop;
+            bullet.BringToFront();
+            form.Controls.Add(bullet);
+
+            bulletTimer.Interval = speed;
+            bulletTimer.Tick += new EventHandler(BulletTimerEvent);
+            bulletTimer.Start();
+        }
+
+        private void BulletTimerEvent(object sender, EventArgs e)
+        {
+            if (direction == "left")
+            {
+                bullet.Left -= speed;
+            }
+
+            if (direction == "right")
+            {
+                bullet.Left += speed;
+            }
+
+            if (direction == "up")
+            {
+                bullet.Top -= speed;
+            }
+
+            if (direction == "down")
+            {
+                bullet.Top += speed;
+            }
+
+            if (bullet.Left < 10 || bullet.Left > 860 || bullet.Top < 10 || bullet.Top > 600) // ako bullet premine odredeni dimenzii da se izbrise
+            {
+                bulletTimer.Stop();
+                bulletTimer.Dispose();
+                bullet.Dispose();
+                bulletTimer = null;
+                bullet = null;
+            }
+        }
     }
 }
