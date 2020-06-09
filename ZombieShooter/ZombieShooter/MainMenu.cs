1184df0818e8648edaa02a046a2cadf50de5914c
Никола Тасевски <nikolatasevski14@gmail.com>
2020-06-09 19:03:56 +0200

    Main-menu-setup

--- ZombieShooter/ZombieShooter/MainMenu.cs
+++ ZombieShooter/ZombieShooter/MainMenu.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace ZombieShooter
 {
-    public partial class Form1 : Form
+    public partial class MainMenu : Form
     {
-        public Form1()
+        public MainMenu()
         {
             InitializeComponent();
         }
+
     }
 }
